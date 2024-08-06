86df9f792e51bb99ce476897391b1e7962c10df5
NezBut <xyi12312354@mail.ru>
2024-08-07 02:22:15 +0700

    feat(core/interfaces/interfaces/scheduler): added an interface for scheduler

---
+++ aiogram_nats/core/interfaces/interfaces/scheduler.py
@@ -0,0 +1,33 @@
+from collections.abc import Awaitable, Callable
+from typing import Any, Protocol, TypeVar
+
+from aiogram_nats.core.entities.scheduled import ScheduledEntity
+
+SupportScheduled = TypeVar("SupportScheduled", bound=ScheduledEntity)
+
+
+class Scheduler(Protocol):
+
+    """
+
+    A protocol for scheduling tasks.
+
+    This class defines the interface for scheduling tasks. It has one method, which takes an `entity` and a `task` as arguments and returns a scheduling ID.
+
+    """
+
+    async def schedule(self, entity: SupportScheduled, task: Callable[[SupportScheduled], Awaitable[Any]]) -> str:
+        """
+        Schedules a task to be executed at a later time.
+
+        Args :
+            entity (SupportScheduled): The entity to be
+             scheduled
+            task (Callable[[SupportScheduled, Any], Awaitable[Any]]): The task to be executed. It should take a `ScheduledEntity` object and any other arguments
+            and return an `Awaitable` object.
+
+        Returns :
+            str: The scheduling ID.
+
+        """
+        raise NotImplementedError
