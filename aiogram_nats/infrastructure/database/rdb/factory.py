7dfc4f758d5391c9e7a8b9937e858f868a95279a
NezBut <xyi12312354@mail.ru>
2024-08-30 15:34:40 +0700

    refactor(infrastructure/di): changed to fit the new settings

--- aiogram_nats/infrastructure/database/rdb/factory.py
+++ aiogram_nats/infrastructure/database/rdb/factory.py
@@ -6,20 +6,20 @@ from sqlalchemy.ext.asyncio import (
     create_async_engine,
 )
 
-from aiogram_nats.common.settings.models.rdb import DBSettings
+from aiogram_nats.common.settings.models.db import RDBSettings
 
 
-def create_engine(config: DBSettings) -> AsyncEngine:
+def create_engine(settings: RDBSettings) -> AsyncEngine:
     """
     Creates an asynchronous database engine based on the provided configuration.
 
     Args :
-        config (DBSettings): The database settings to use for engine creation.
+        settings (RDBSettings): The relational database settings to use for engine creation.
 
     Returns :
         AsyncEngine: The created asynchronous database engine.
     """
-    return create_async_engine(url=make_url(config.make_uri().value), echo=config.echo)
+    return create_async_engine(url=make_url(settings.make_uri().value), echo=settings.echo)
 
 
 def create_session_maker(engine: AsyncEngine) -> async_sessionmaker[AsyncSession]:
