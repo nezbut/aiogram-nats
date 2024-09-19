e31f6df4b9fcbb6f64d5756ce5c66eefd89b5bb8
NezBut <xyi12312354@mail.ru>
2024-09-19 21:28:39 +0700

    feat(infrastructure/database): add mapper

--- aiogram_nats/infrastructure/database/mappers/rdb.py
+++ aiogram_nats/infrastructure/database/mappers/rdb.py
@@ -1,10 +1,19 @@
+from adaptix import dump
 from adaptix.conversion import get_converter
 
 from aiogram_nats.core.entities.user import User as UserEntity
-from aiogram_nats.infrastructure.database.rdb.models.user import User
+from aiogram_nats.infrastructure.database.rdb.models.user import UserORM
 
-userdb_to_entity = get_converter(User, UserEntity)
+userdb_to_entity = get_converter(UserORM, UserEntity)
+
+
+def user_entity_to_db(user: UserEntity) -> UserORM:
+    """Converts a UserEntity object to a UserORM object."""
+    user_data = dump(user)
+    return UserORM(**user_data)
+
 
 __all__ = [
     "userdb_to_entity",
+    "user_entity_to_db",
 ]
