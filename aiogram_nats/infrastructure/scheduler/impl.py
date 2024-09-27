5eed4473f0e61785c0013dab2af0f8b9ca001bcc
NezBut <xyi12312354@mail.ru>
2024-09-27 23:30:52 +0700

    feat(infrastructure): add logging to scheduler

--- aiogram_nats/infrastructure/scheduler/impl.py
+++ aiogram_nats/infrastructure/scheduler/impl.py
@@ -3,6 +3,8 @@ from typing import Any, TypeVar
 import taskiq.scheduler.created_schedule
 from taskiq import AsyncTaskiqDecoratedTask, ScheduleSource
 
+from aiogram_nats.common.log.configuration import LoggerName
+from aiogram_nats.common.log.installer import LoggersInstaller
 from aiogram_nats.core.entities.mailing import ScheduledMailing
 from aiogram_nats.core.entities.message import MessageDeletionScheduled, MessageSendScheduled
 from aiogram_nats.core.entities.scheduled import ScheduledEntity
@@ -21,8 +23,13 @@ class SchedulerImpl(Scheduler):
     allows you to schedule tasks to be executed at a later time.
     """
 
-    def __init__(self, source: ScheduleSource) -> None:
+    def __init__(self, source: ScheduleSource, logging: LoggersInstaller) -> None:
         self.source = source
+        self.logger = logging.get_logger(
+            LoggerName.SCHEDULER,
+            source=source.__class__.__name__,
+        )
+        self._max_text_len = 20
 
     async def _schedule(self, task: AsyncTaskiqDecoratedTask[[ScheduledSupport], Any], entity: ScheduledSupport) -> str:
         scheduled: taskiq.scheduler.created_schedule.CreatedSchedule = await task.schedule_by_time(
@@ -42,7 +49,16 @@ class SchedulerImpl(Scheduler):
         :return: The scheduling ID of the send task.
         :rtype: str
         """
-        return await self._schedule(tasks.send_message, message)
+        task_id = await self._schedule(tasks.send_message, message)
+        await self.logger.adebug(
+            "Schedule send message: address=%s, text=%s, time=%s, task=%s",
+            message.media_address,
+            message.text[:self._max_text_len] +
+            "..." if len(message.text) > self._max_text_len else message.text,
+            message.scheduled_time,
+            task_id,
+        )
+        return task_id
 
     async def schedule_delete_message(self, message: MessageDeletionScheduled) -> str:
         """
@@ -54,7 +70,14 @@ class SchedulerImpl(Scheduler):
         :return: The scheduling ID of the deletion task.
         :rtype: str
         """
-        return await self._schedule(tasks.remove_message, message)
+        task_id = await self._schedule(tasks.remove_message, message)
+        await self.logger.adebug(
+            "Schedule delete message: id=%s, time=%s, task=%s",
+            message.id,
+            message.scheduled_time,
+            task_id,
+        )
+        return task_id
 
     async def schedule_mailing(self, mailing: ScheduledMailing) -> str:
         """
@@ -66,4 +89,16 @@ class SchedulerImpl(Scheduler):
         :return: The scheduling ID of the mailing.
         :rtype: str
         """
-        return await self._schedule(tasks.start_mailing, mailing)
+        message = f"Text: {mailing.message.text} \
+            {f'Media(address={mailing.message.media.address}, type={mailing.message.media.media_type})' if mailing.message.media else ''}"
+        task_id = await self._schedule(tasks.start_mailing, mailing)
+        await self.logger.adebug(
+            "Schedule mailing: Mailing(id=%s, creator=User(id=%s, name=%s), time=%s, message=%s). Task id: %s",
+            mailing.id,
+            mailing.creator.id,
+            mailing.creator.name,
+            mailing.scheduled_time,
+            message,
+            task_id,
+        )
+        return task_id
